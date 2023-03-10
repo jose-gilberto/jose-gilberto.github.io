74f311d7bf70a083f00c2d4f49ca6dc4d8bcabd7
Gilberto Barbosa <medeiros.gilberto.br@gmail.com>
2023-03-09 22:02:53 -0300

    Fix Script import

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -7,11 +8,11 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script
+        <Script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
           crossOrigin="anonymous">
-        </script>
+        </Script>
       </body>
     </Html>
   )
