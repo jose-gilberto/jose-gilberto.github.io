fd19d73663817af0f0e74f82d3f9e5e51f1fbedd
Gilberto Barbosa <medeiros.gilberto.br@gmail.com>
2023-03-09 21:50:35 -0300

    Fix GH action

--- next.config.js
+++ next.config.js
@@ -1,22 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+const nextConfig   = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
+};
 
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  // images: {
-  //   loader: 'imgix',
-  //   path: 'the "domain" of your Imigix source',
-  // },
-}
+export default   nextConfig;
